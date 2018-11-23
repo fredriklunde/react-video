443c038b454db4943ebfe0425aa763cd3371d5f0
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-23 16:42:04 +0100

    some style

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,9 @@
+import './VideoItem.css';
 import React from 'react';
 
 const VideoItem = ({video}) => {
     return (
-        <div className="item">
+        <div className="video-item item">
             <img className="ui image" src={video.snippet.thumbnails.medium.url} />
             <div className="content">
                 <div className="header">
