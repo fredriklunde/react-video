b692738c879ea9020173f82df673f3f502d2d97a
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-23 16:05:22 +0100

    youtube req

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -7,6 +7,6 @@ export default axios.create({
     params: {
         part: 'snippet',
         maxResults: 5,
-        key=youtubeApiKey
+        key: youtubeApiKey
     }
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
     onTermSubmit = (term) => {
-        console.log(term);
+        youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
     };
 
     render() {
