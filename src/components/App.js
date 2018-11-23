9b53fb5b343a7f4870d8c3c0f24dd11a19ed0a70
Fredrik Lunde <fredrik.lunde3@gmail.com>
2018-11-23 15:34:10 +0100

    new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
     render() {
-        return <div> App </div>;
+        return (
+            <div>
+                App 
+                <SearchBar />
+            </div>
+        );
     }
 }
 
