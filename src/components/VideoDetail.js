64143003d2792cc35810ebd897197b21da1229f4
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-25 17:37:22 +0100

    embed vidoe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
     if(!video) {
-        return <div>loading</div>;
+        return <div>Loading...</div>;
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
         <div>
+            <div className="ui embed">
+                <iframe title={video.snippet.title} src={videoSrc} />
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
-                <p>
-                    {video.snippet.description}
-                </p>
+                <p>{video.snippet.description}</p>
             </div>
         </div>
     )
