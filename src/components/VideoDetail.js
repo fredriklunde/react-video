0cd9c8197a376e08ef528b0e818cf2fff84da9a1
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-25 17:44:01 +0100

    fixing errors

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -10,7 +10,7 @@ const VideoDetail = ({ video }) => {
     return (
         <div>
             <div className="ui embed">
-                <iframe title={video.snippet.title} src={videoSrc} />
+                <iframe title="video player" src={videoSrc} />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">{video.snippet.title}</h4>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video} onVideoSelect={onVideoSelect} />
+        return (
+            <VideoItem 
+                key={video.id.videoId} 
+                video={video} 
+                onVideoSelect={onVideoSelect} 
+            />
+        );
     });
     
     return (
