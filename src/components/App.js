d25f63083c840b55f4464aecd4e3c0f7ad27e029
fredriklunde <fredrik.lunde3@gmail.com>
2018-11-25 17:28:35 +0100

    some new stuff

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = { 
         videos: [],
-        selectedVideo: {}
+        selectedVideo: null
     };
 
     onTermSubmit = async (term) => {
@@ -26,6 +27,7 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
+    if(!video) {
+        return (
+            <div>
+                loading
+            </div>
+        );
+    }
     return (
         <div>
             {video.snippet.title}
